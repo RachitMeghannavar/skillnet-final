28ec5971fe159e33392290769c6b4d801a2f6ae7
RachitMeghannavar <rachitmeghannavar8@gmail.com>
2026-05-26 00:45:20 +0530

    Add files via upload

---
+++ skillnet.js
@@ -0,0 +1,12 @@
+  function openSignin(){
+    window.location.href="signup.html";
+  }
+  function linkpro(){
+    window.location.href="create_profile.html";
+  }
+  function opentech(){
+    window.location.href="teachnicalevent.html";
+  }
+  function opencources(){
+    window.location.href="certification.html";
+  }
---
+++ startup.js
@@ -0,0 +1,21 @@
+function handleSubmit(event) {
+    let startup = document.getElementById('inputType1').value;
+    localStorage.setItem('inputType1', startup);
+window.location.href = 'startupout.html';
+}
+let storedstartup = localStorage.getItem('inputType1');
+document.getElementById('inputType1').innerHTML = storedstartup;
+function handleSubmit(event) {
+    let startup = document.getElementById('inputType2').value;
+    localStorage.setItem('inputType2', brief);
+window.location.href = 'startupout.html';
+}
+let storedstartup = localStorage.getItem('inputType2');
+document.getElementById('inputType2').innerHTML = storedstartup;function handleSubmit(event) {
+    let startup = document.getElementById('inputType2').value;
+    localStorage.setItem('inputType2', brief);
+window.location.href = 'startupout.html';
+}
+let storedstartup = localStorage.getItem('inputType2');
+document.getElementById('inputType2').innerHTML = storedstartup;
+window.location.href = 'startupout.html';
